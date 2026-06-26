8b0e91e1ab598c3db3a82f5c47012b1489b97f2f
Ifeouwa Olujobi <ifeoluwaolujobi@gmail.com>
2026-06-26 07:20:00 +0100

    Display locations on UI if we have multiple locations

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import "../styles/App.css";
 import SearchForm from "./SearchForm";
 import LocationResults from "./LocationResults.jsx";
+import "../styles/location.css";
 
 const GEOCODE_URL = "https://geocoding-api.open-meteo.com/v1/search";
 const WEATHER_URL = "https://api.open-meteo.com/v1/forecast";
@@ -10,7 +11,7 @@ function App() {
   const [formInput, setFormInput] = useState("");
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState("");
-  const [location, setLocation] = useState([]);
+  const [locations, setLocations] = useState([]);
   const [weather, setWeather] = useState({});
 
   async function handleSubmit(e) {
@@ -20,9 +21,13 @@ function App() {
 
     setLoading(true);
     setError("");
-    setLocation([]);
+    setLocations([]);
     setWeather({});
 
+    fetchLocationData();
+  }
+
+  async function fetchLocationData() {
     try {
       const response = await fetch(`${GEOCODE_URL}?name=${formInput}&count=5`);
 
@@ -33,25 +38,39 @@ function App() {
 
       //If results does not exists since API always returns 200, I had to do a manual error handling
       if (!data.results || data.results.length === 0)
-        throw new Error("Location does not exist");
+        throw new Error("City not found");
 
       if (data.results.length > 1) {
-        setLocation([data.results]);
+        setLocations(data.results);
         return;
       }
 
       //If location is exactly one
+      fetchWeatherData(data.results[0].latitude, data.results[0].longitude);
+    } catch (error) {
+      console.log(error);
+      setError(`${error.message}`);
+    } finally {
+      setLoading(false);
+    }
+  }
 
+  async function fetchWeatherData(latitude, longitude) {
+    setLoading(true);
+    setError("");
+    setLocations([]);
+    setWeather({});
+
+    try {
       const weatherResponse = await fetch(
-        `${WEATHER_URL}?latitude=${data.results[0].latitude}&longitude=${data.results[0].longitude}&current=temperature_2m,wind_speed_10m,relative_humidity_2m`,
+        `${WEATHER_URL}?latitude=${latitude}&longitude=${longitude}&current=temperature_2m,wind_speed_10m,relative_humidity_2m`,
       );
 
       const weatherData = await weatherResponse.json();
       console.log(weatherResponse);
       console.log(weatherData);
     } catch (error) {
-      console.log(error);
-      setError("Error occured: ", error);
+      setError(`${error.message}`);
     } finally {
       setLoading(false);
     }
@@ -65,8 +84,15 @@ function App() {
         onHandleSubmit={handleSubmit}
       />
       <main>
+        {error && <p>{error}</p>}
         {loading && <p>Loading..... </p>}
-        {location.length > 0 && <LocationResults location={location} />}
+        {locations.length > 0 && (
+          <section className='location-grid-container'>
+            {locations.map((location) => (
+              <LocationResults key={location.id} location={location} />
+            ))}
+          </section>
+        )}
       </main>
     </>
   );
--- src/components/LocationResults.jsx
+++ src/components/LocationResults.jsx
@@ -1,5 +1,17 @@
-function LocationResults() {
-  return <div>LocationResults</div>;
+import "../styles/location.css";
+
+function LocationResults({ location }) {
+  return (
+    <div className='location-container'>
+      <div>
+        <p>{location.name}</p>
+        <p>{location.country}</p>
+        <p>{location.timezone}</p>
+      </div>
+
+      <button>Select</button>
+    </div>
+  );
 }
 
 export default LocationResults;
